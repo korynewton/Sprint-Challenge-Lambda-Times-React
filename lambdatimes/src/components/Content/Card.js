0825d8e1f8f00c143e1fe144560a801c55b6d872
korynewton <korynewtondev@gmail.com>
2019-02-22 10:25:55 -0800

    Header converted to styled components, cleaned up some warnings, mvp complete

--- lambdatimes/src/components/Content/Card.js
+++ lambdatimes/src/components/Content/Card.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Cards from './Cards';
+// import Cards from './Cards';
 import PropTypes from 'prop-types'
 
 const Card = props => {
@@ -9,7 +9,7 @@ const Card = props => {
       <div className="headline">{props.card.headline}</div>
       <div className="author">
         <div className="img-container">
-          <img src={props.card.img /* image source goes here */} />
+          <img src={props.card.img /* image source goes here */} alt={props.headline} />
         </div>
         <span>By {props.card.author/* author goes here */}</span>
       </div>
