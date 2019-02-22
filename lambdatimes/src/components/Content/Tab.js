6292e1e41a463c0773eb0d11d0636ff33563f4d1
korynewton <korynewtondev@gmail.com>
2019-02-22 09:04:17 -0800

    Tab component complete

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,22 +1,51 @@
 import React from 'react';
+import PropTypes from 'prop-types'
+
+
+
 
 const Tab = props => {
+  console.log(props)
+
   /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
       if they match, the className should be: 'tab active-tab', 
       if it is not it should just be 'tab'*/
+      if (props.tab === props.selectedTab) {
+        return (
+          <div
+            className={'tab active-tab'}
+            onClick={(tab) => {props.selectTabHandler(tab)}
+              /* Replace this dummy click handler function with your selectTabHandler function from props 
+               you'll need to pass the `tab` in as an argument to this handler. */
+            }
+          >
+            {props.tab.toUpperCase()}
+          </div>
+        );
+      }
+      else {
+
+      
   return (
-    <div
-      className={''}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }}
-    >
-      {props.tab.toUpperCase()}
-    </div>
-  );
-};
+      <div
+        className={'tab'}
+        onClick={(tab) => {props.selectTabHandler(tab)}
+          /* Replace this dummy click handler function with your selectTabHandler function from props 
+            you'll need to pass the `tab` in as an argument to this handler. */
+        }
+      >
+        {props.tab.toUpperCase()}
+      </div>
+    );
+    };
+  }
 
 // Make sure you include PropTypes on your props.
+Tab.propTypes = {
+  tab: PropTypes.string.isRequired,
+  selectedTab: PropTypes.string.isRequired,
+  selectTabHandler: PropTypes.func.isRequired
+
+}
 
 export default Tab;
